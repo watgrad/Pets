bb26b716d6864fc69b6bb5ad153510890f38e7f4
John L <watgrad@gmail.com>
2019-02-10 18:54:19 -0500

    updates

--- myPets.py
+++ myPets.py
@@ -113,10 +113,104 @@ class ViewPet(tk.Frame):
         date_data = ttk.Label(stats_frame, text = curr_stats[0][5], font=text_font)
         date_data.grid(row=7, column=1, sticky="ws")
 
+        mlb_label = ttk.LabelFrame(main_tab_frame, text="Vet Visits")
+        mlb_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+        mlb = MultiListbox(mlb_label, (('Rec #', 5), ('Date', 10), ('Appointment', 15), ('Cost', 8)))
+        for i in curr_vet:
+            mlb.insert("end", (i[0], i[2], i[3], i[4]))
+        mlb.pack()
+
 
 class AddPet(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent) #initialize the Frame parent
+
+
+class MultiListbox(tk.Frame):
+    def __init__(self, master, lists):
+        tk.Frame.__init__(self, master)
+        self.lists = []
+        for l,w in lists:
+            frame = tk.Frame(self); frame.pack(side="left", expand="yes", fill="both")
+            tk.Label(frame, text=l, borderwidth=1, relief="raised", font=label_font).pack(fill='x')
+            lb = tk.Listbox(frame, width=w, borderwidth=0, selectborderwidth=0,
+                         relief="flat", exportselection=False, font=text_font)
+            lb.pack(expand="yes", fill="both")
+            self.lists.append(lb)
+            lb.bind('<B1-Motion>', lambda e, s=self: s._select(e.y))
+            lb.bind('<Button-1>', lambda e, s=self: s._select(e.y))
+            lb.bind('<Leave>', lambda e: 'break')
+            lb.bind('<B2-Motion>', lambda e, s=self: s._b2motion(e.x, e.y))
+            lb.bind('<Button-2>', lambda e, s=self: s._button2(e.x, e.y))
+        frame = tk.Frame(self); frame.pack(side="left", fill="y")
+        tk.Label(frame, borderwidth=1, relief="raised", font=text_font).pack(fill="x")
+        sb = tk.Scrollbar(frame, orient="vertical", command=self._scroll)
+        sb.pack(expand="yes", fill="y")
+        self.lists[0]['yscrollcommand']=sb.set
+
+    def _select(self, y):
+        row = self.lists[0].nearest(y)
+        self.selection_clear(0, 'end')
+        self.selection_set(row)
+        return 'break'
+
+    def _button2(self, x, y):
+        for l in self.lists: l.scan_mark(x, y)
+        return 'break'
+
+    def _b2motion(self, x, y):
+        for l in self.lists: l.scan_dragto(x, y)
+        return 'break'
+
+    def _scroll(self, *args):
+        for l in self.lists:
+            apply(l.yview, args)
+
+    def curselection(self):
+        return self.lists[0].curselection(  )
+
+    def delete(self, first, last=None):
+        for l in self.lists:
+            l.delete(first, last)
+
+    def get(self, first, last=None):
+        result = []
+        for l in self.lists:
+            result.append(l.get(first,last))
+        if last: return apply(map, [None] + result)
+        return result
+
+    def index(self, index):
+        self.lists[0].index(index)
+
+    def insert(self, index, *elements):
+        for e in elements:
+            i = 0
+            for l in self.lists:
+                l.insert(index, e[i])
+                i = i + 1
+
+    def size(self):
+        return self.lists[0].size(  )
+
+    def see(self, index):
+        for l in self.lists:
+            l.see(index)
+
+    def selection_anchor(self, index):
+        for l in self.lists:
+            l.selection_anchor(index)
+
+    def selection_clear(self, first, last=None):
+        for l in self.lists:
+            l.selection_clear(first, last)
+
+    def selection_includes(self, index):
+        return self.lists[0].selection_includes(index)
+
+    def selection_set(self, first, last=None):
+        for l in self.lists:
+            l.selection_set(first, last)
 
 
 if __name__ == "__main__":
