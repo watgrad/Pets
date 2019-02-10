0d1f0987e2acba8dd836d2833de276737b171fe6
John L <watgrad@gmail.com>
2019-02-10 15:27:37 -0500

    working on it

--- myPets.py
+++ myPets.py
@@ -44,24 +44,26 @@ class myPetDB(tk.Tk):
 class ViewPet(tk.Frame):
     
     def __init__(self, parent, controller):
+        
+        # Get Data to fill page ==>  must be replaced in future calls
         curr_pet = pet_db.find_records('pets', 7)
         curr_stats = pet_db.find_records('stats', 7)
         curr_pic = pet_db.find_records('images', 7)
+        curr_vet = pet_db.find_records('vet', 7)
         print(curr_pet)
         print(curr_stats)
         print(curr_pic)
 
         tk.Frame.__init__(self,parent) #initialize the Frame parent
         my_tabs = ttk.Notebook(self)
-        #my_tabs.grid(row=1, column=0, padx=5, pady=5, columnspan=50, rowspan=49, sticky='NSEW')
         my_tabs.pack(anchor='n', fill='both', expand=True, padx=5, pady=5)
         main_tab = ttk.Frame(my_tabs)
         
         my_tabs.add(main_tab, text='My Pets', sticky='NSEW')
         main_tab_frame = ttk.LabelFrame(main_tab, text=curr_pet[0][1])
-        #main_tab_frame.grid(row=1, column=0, padx=5, pady=5)
         main_tab_frame.pack(anchor='n', fill='both', expand=True, padx=5, pady=5)
 
+        #-------------IMAGE
         my_image = Image.open(curr_pic[0][1])
         w, h = my_image.size
         f = h//200
@@ -71,25 +73,45 @@ class ViewPet(tk.Frame):
         pet_image = tk.Label(main_tab_frame, image=my_photo) 
         pet_image.image = my_photo  # strangely, you must keep a reference to the Tkinter object, for example by attaching it to a widget attribute
         pet_image.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-        stats_frame = ttk.LabelFrame(main_tab_frame, text="Molly's Stats")
-        stats_frame.grid(row=0, column=1, padx=5, pady=5, sticky='nsew')
+        
+        #-------------STATS
+        stats_frame = ttk.LabelFrame(main_tab_frame, text=(curr_pet[0][1]+"'s Stats"))
+        stats_frame.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
         
         dob_label = ttk.Label(stats_frame, text = "D.O.B. ", font = label_font)
-        dob_label.grid(row=0, column=0, sticky='e')
+        dob_label.grid(row=0, column=0, sticky="e")
         dob_data = ttk.Label(stats_frame, text = curr_pet[0][1], font=text_font)
         dob_data.grid(row=0, column=1, sticky="w")
 
         breed_label = ttk.Label(stats_frame, text = "Breed ", font = label_font)
-        breed_label.grid(row=1, column=0, sticky='e')
+        breed_label.grid(row=1, column=0, sticky="e")
         breed_data = ttk.Label(stats_frame, text = curr_pet[0][3], font=text_font)
         breed_data.grid(row=1, column=1, sticky="w")
 
         sex_label = ttk.Label(stats_frame, text = "Sex ", font = label_font)
-        sex_label.grid(row=2, column=0, sticky='e')
+        sex_label.grid(row=2, column=0, sticky="e")
         sex_data = ttk.Label(stats_frame, text = curr_pet[0][4], font=text_font)
         sex_data.grid(row=2, column=1, sticky="w")
 
+        weight_label = ttk.Label(stats_frame, text = "Weight ", font = label_font)
+        weight_label.grid(row=4, column=0, sticky="e")
+        weight_data = ttk.Label(stats_frame, text = curr_stats[0][1], font=text_font)
+        weight_data.grid(row=4, column=1, sticky="w")
 
+        girth_label = ttk.Label(stats_frame, text = "Girth ", font = label_font)
+        girth_label.grid(row=5, column=0, sticky="e")
+        girth_data = ttk.Label(stats_frame, text = curr_stats[0][2], font=text_font)
+        girth_data.grid(row=5, column=1, sticky="w")
+
+        length_label = ttk.Label(stats_frame, text = "Length ", font = label_font)
+        length_label.grid(row=6, column=0, sticky="e")
+        length_data = ttk.Label(stats_frame, text = curr_stats[0][3], font=text_font)
+        length_data.grid(row=6, column=1, sticky="w")
+
+        date_label = ttk.Label(stats_frame, text = "Taken on ", font = label_font)
+        date_label.grid(row=7, column=0, sticky="es")
+        date_data = ttk.Label(stats_frame, text = curr_stats[0][5], font=text_font)
+        date_data.grid(row=7, column=1, sticky="ws")
 
 
 class AddPet(tk.Frame):
